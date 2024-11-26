9574f686964cdedd09256eaed97525025128943a
Florian <floriangeorges1992@gmail.com>
2024-11-26 15:39:04 +0100

    add litteraux.js

--- src/chaines.js
+++ src/chaines.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const a = "soleil";
+const b = String(55);
+//creation d'un objet String
+const c = new String("terre");
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+const d = "astre: " + "soleil";
+console.log(d);
